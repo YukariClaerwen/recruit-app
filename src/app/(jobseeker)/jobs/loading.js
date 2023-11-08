8258fb770f7d016972077060c8e29d70d8bd0a4d
YukariClaerwen <yukari.8239@gmail.com>
2023-11-08 13:31:00 +0700

    edit jobs

---
+++ src/app/(jobseeker)/jobs/loading.js
@@ -0,0 +1,15 @@
+
+const loading = () => {
+  return (
+    
+    <div className="spinner-container z-[2000] flex justify-center items-center">
+    <div className="loading-jumping-dots flex justify-center items-center" role="status">
+      <span className="dot"></span>
+      <span className="dot"></span>
+      <span className="dot"></span>
+    </div>
+  </div>
+  )
+}
+
+export default loading

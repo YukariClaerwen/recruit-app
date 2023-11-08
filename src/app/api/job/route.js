3b4c52c20a180a9f7fcdd568f759918f2e1ef14a
YukariClaerwen <yukari.8239@gmail.com>
2023-11-08 21:49:57 +0700

    fix bug api

--- src/app/api/job/route.js
+++ src/app/api/job/route.js
@@ -1,23 +1,23 @@
 import { db } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request) {
-    try {
-        const formData = await request.formData();
-        const data = {
-            key: formData.get('frmSearchKey'),
-            location: formData.get('frmSearchLocation')
-        }
+// export async function GET(request) {
+//     try {
+//         const formData = await request.formData();
+//         const data = {
+//             key: formData.get('frmSearchKey'),
+//             location: formData.get('frmSearchLocation')
+//         }
 
-        const result = await db.viecLam.findMany({ 
-            where: {
-                 mo_ta : {search: data.key}
-            }
-        })
+//         const result = await db.viecLam.findMany({ 
+//             where: {
+//                  mo_ta : {search: data.key}
+//             }
+//         })
 
-        return NextResponse.json({ result }, { status: 201 });
-    } catch (err) {
-        throw new Error(err);
-        // return NextResponse.json( { message: "Something went wrong"}, { status: 500 });
-    }
-}
+//         return NextResponse.json({ result }, { status: 201 });
+//     } catch (err) {
+//         throw new Error(err);
+//         // return NextResponse.json( { message: "Something went wrong"}, { status: 500 });
+//     }
+// }
